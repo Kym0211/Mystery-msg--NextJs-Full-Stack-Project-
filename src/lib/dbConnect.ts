7209858eb8c94e99d25897a1e72a5f1d048196fa
Kavyam <kavyam.ksingh@gmail.com>
2024-08-24 05:04:06 +0530

    chore: Add environment variables and schemas for sign-in, accept message, verify, and sign-up

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        log("Already connected to database")
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+
+        log(db);
+        connection.isConnected = db.connections[0].readyState
+        log("DB Connected Successfully")
+    } catch (error) {
+        log("DB Connection Error: ", error)
+        process.exit()
+    }
+}
+
+export default dbConnect;
